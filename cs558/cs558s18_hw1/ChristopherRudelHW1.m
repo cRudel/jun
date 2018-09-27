456e0d043f0019dce6f6d75014fa68a6e30a10b7
Chris Rudel <crudel@stevens.edu>
2018-09-27 00:56:25 -0400

    558 problems 1&2

--- cs558/cs558s18_hw1/ChristopherRudelHW1.m
+++ cs558/cs558s18_hw1/ChristopherRudelHW1.m
@@ -9,7 +9,10 @@ redSobel = filter('sobel', red, 250);
 planeSobel = filter('sobel', plane, 115);
 kangarooSobel = filter('sobel', kangaroo, 200);
 
-imshow(kangarooGauss);
+%helppp
+nmxPlane = nonMaxSupp(magnitude(plane), direction(plane)); 
+figure,imshow(nmxPlane);
+figure,imshow(imgradient(red));
 end
 
 function f=filter(type,pic,threshold)
@@ -39,17 +42,7 @@ f = Output;
 
 else %sobel filter
     sobP = double(pic);
-    for i=1:size(sobP,1)-2
-        for j=1:size(sobP,2)-2
-        Gx=((2*sobP(i+2,j+1)+sobP(i+2,j)...
-        +sobP(i+2,j+2))-(2*sobP(i,j+1)...
-        +sobP(i,j)+sobP(i,j+2)));
-        Gy=((2*sobP(i+1,j+2)+sobP(i,j+2)+sobP(i+2,j+2))...
-        -(2*sobP(i+1,j)+sobP(i,j)+sobP(i+2,j)));
-        
-        sobP(i,j) = sqrt(Gx.^2+Gy.^2);
-        end
-    end
+    sobP = magnitude(sobP);
     P = max(sobP, threshold);
     P(P==round(threshold))=0;
     P = uint8(P);
@@ -62,6 +55,59 @@ else %sobel filter
 end
 end
 
-function f = nonMaxSupp(dx, dy, gradMag, lowThresh, highThresh)
+function i = magnitude(image)
+    I = double(image);
+    for i=1:size(I,1)-2
+    for j=1:size(I,2)-2
+        Gx=((2*I(i+2,j+1)+I(i+2,j)+I(i+2,j+2))-(2*I(i,j+1)+I(i,j)+I(i,j+2)));
+        Gy=((2*I(i+1,j+2)+I(i,j+2)+I(i+2,j+2))-(2*I(i+1,j)+I(i,j)+I(i+2,j)));
+        grad(i,j)=sqrt(Gx.^2+Gy.^2);
+    end
+    end
+    i = grad;
+    
+end
+
+function i = direction(image)
+    I = double(image);
+    for i=1:size(I,1)-2
+    for j=1:size(I,2)-2
+        Gx=((2*I(i+2,j+1)+I(i+2,j)+I(i+2,j+2))-(2*I(i,j+1)+I(i,j)+I(i,j+2)));
+        Gy=((2*I(i+1,j+2)+I(i,j+2)+I(i+2,j+2))-(2*I(i+1,j)+I(i,j)+I(i+2,j)));
+        theta(i,j) = atand(Gy/Gx);
+    end
+    end
+    i=theta;
+end
+
+
+ function [ marks ] = nonMaxSupp( mag,dir )
+    [m,n]=size(mag);
+    marks=uint8(ones(size(mag)));
+    for i=2:m-1
+        for j=2:n-1
+            if mag(i,j) ~=0
+               angle=dir(i,j)+180;
+               if (angle>=340 || angle<=22.5) || (angle>=157.5 && angle<=202.5)  %horizontal
+                if (mag(i,j+1)>mag(i,j)) || (mag(i,j-1)>mag(i,j))
+                    marks(i,j)=0;
+                end
+
+            elseif (angle>22.5 && angle<=67.5) || (angle>202.5 && angle<=247.5) %45
+                 if (mag(i+1,j+1)>mag(i,j)) || (mag(i-1,j-1)>mag(i,j))
+                    marks(i,j)=0;
+                 end
+            elseif (angle>67.5 && angle<=112.5) || (angle>247.5 && angle<=292.5) %vertical
+                 if (mag(i-1,j)>mag(i,j)) ||(mag(i+1,j)>mag(i,j))
+                    marks(i,j)=0;
+                end
+            else %135
+                 if (mag(i-1,j+1)>mag(i,j)) || (mag(i+1,j-1)>mag(i,j))
+                    marks(i,j)=0;
+                 end
+               end
+            end
+        end 
+    end
 
 end
