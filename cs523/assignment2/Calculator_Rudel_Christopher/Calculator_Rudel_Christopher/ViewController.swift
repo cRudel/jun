0ccb39b54b55eaab5661007943c62fbd2da13926
Chris Rudel <crudel@stevens.edu>
2019-01-29 15:19:57 -0500

    Started working on the calculator

--- cs523/assignment2/Calculator_Rudel_Christopher/Calculator_Rudel_Christopher/ViewController.swift
+++ cs523/assignment2/Calculator_Rudel_Christopher/Calculator_Rudel_Christopher/ViewController.swift
@@ -10,6 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var firstNumber = ""
+    var secondNumber = ""
+    var operand = ""
+    var result:String = ""
+    
+    @IBOutlet weak var myOutputLabel: UILabel!
+    
+    
+    @IBAction func myZero(_ sender: Any) {
+        
+    }
+    @IBAction func myOne(_ sender: Any) {
+        myOutputLabel.text = "TODO: finish this up"
+    }
+    @IBAction func myTwo(_ sender: Any) {
+        
+    }
+    @IBAction func myThree(_ sender: Any) {
+        
+    }
+    @IBAction func myFour(_ sender: Any) {
+        
+    }
+    @IBAction func myFive(_ sender: Any) {
+        
+    }
+    @IBAction func mySix(_ sender: Any) {
+        
+    }
+    @IBAction func mySeven(_ sender: Any) {
+        
+    }
+    @IBAction func myEight(_ sender: Any) {
+        
+    }
+    @IBAction func myNine(_ sender: Any) {
+        
+    }
+    
+    @IBAction func myEquals(_ sender: Any) {
+        
+    }
+    
+    @IBAction func myClear(_ sender: Any) {
+        myOutputLabel.text = ""
+        result = ""
+        firstNumber = ""
+        secondNumber = ""
+        operand = ""
+        result = ""
+    }
+    
+    @IBAction func myAddition(_ sender: Any) {
+    }
+    @IBAction func mySubtraction(_ sender: Any) {
+    }
+    @IBAction func myDivision(_ sender: Any) {
+    }
+    @IBAction func myMultiplication(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
