c2d591f95ff0eb3e7d0421d65fb84a26d7f10978
Chris Rudel <crudel@stevens.edu>
2019-02-08 15:15:21 -0500

    Finished lab2 546

--- cs546/labs/lab2/index.js
+++ cs546/labs/lab2/index.js
@@ -35,8 +35,40 @@ try{
 }
 try{
     //should fail
-    const removeTwo = arrayUtils.remove([1,2,3,4], 4);
+    const removeTwo = arrayUtils.remove([1,2,3,4], 4); // 4 is the length of the array, bigger than an index
     console.error("Remove did not error");
 }catch(e){
     console.log("Remove failed successfully ")
+}
+
+// countChars test
+try{
+    // should pass
+    const countOne = stringUtils.countChars('I love web programming');
+    console.log("countChars passed successfully");
+}catch(e){
+    console.error("countChars test case failed");
+}
+try{
+    // should fail
+    const countTwo = stringUtils.countChars(false);
+    console.error("countChars did not error");
+}catch(e){
+    console.log("countChars failed successfully");
+}
+
+// extend test
+try{
+    // should pass
+    const extendOne = objUtils.extend({ a: 70, x: 4, z: 5 }, { x: 0, y: 9, q: 10 });
+    console.log("Extend passed successfully");
+}catch(e){
+    console.error("Extend test case failed");
+}
+try{
+    // should fail
+    const extendTwo = objUtils.extend({ a: 70, x: 4, z: 5}, [1,2,3,4]);
+    console.error("Extend did not error");
+}catch(e){
+    console.log("Extend failed successfully");
 }
