94006ff0062864fb6f07ff2478ec255426ebca3c
BuildTools <cRudel@github.com>
2019-02-18 22:57:52 -0500

    Finished problems for lab3. Testing needs to be done but should be done for the most part

--- cs546/labs/lab3/app.js
+++ cs546/labs/lab3/app.js
@@ -10,4 +10,16 @@ const people = require("./people");
 const weather = require("./weather");
 const work = require("./work");
 
-let test = people.getPersonById("");
+const start = async function start(){
+    try{
+        //const test = await people.firstNameMetrics();
+        //console.log(test);
+
+        let test = await work.findTheHacker("157.254.170.156");
+        console.log(test);
+    }catch(error){
+        console.error(error);
+    }
+}
+
+start();
