9dd4259025484674af93625fac403a7253326c29
Chris Rudel <crudel@stevens.edu>
2019-02-27 11:03:31 -0500

    in class stuff

---
+++ cs523/RunningLate/RunningLate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RunningLate
+//
+//  Created by Chris Rudel on 2/25/19.
+//  Copyright © 2019 Chris Rudel. All rights reserved.
+//
+//  Still need to design the core functionality of the app
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func sidebarBtn(_ sender: Any) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
