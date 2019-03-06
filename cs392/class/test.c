d16938bbe53e7fe8f5607c68e35ecd0d7a0bfc0c
Chris Rudel <crudel@stevens.edu>
2019-03-06 15:12:35 -0500

    cs392 midterm completed

--- cs392/class/test.c
+++ cs392/class/test.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-
+#include <string.h>
 int main(){
 
-	char str[10] = "hello";
-	printf("%lu\n", sizeof(str));
-	str[5] = '\0';
-	str[6] = 'o';
-	for(int i=0; i< 10; i++)
-		printf("%c\n", str[i]);
-	printf("%lu\n", sizeof(str));
-
+	char src[] = "hello";
+	char dest[10];
+	strcpy(dest,src);
+	int i=0;
+	while(dest[i] != '\0'){
+		i++;
+	}
+	printf("%d\n",i);
 	return 0;
 }
+
