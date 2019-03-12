2b5edd2b02f30462089a424887302cb9c032a736
Chris Rudel <crudel@stevens.edu>
2019-03-12 15:04:48 -0400

    cs392 assignment 2 finished

---
+++ cs392/as2/cs392_string.h
@@ -0,0 +1,16 @@
+/*
+*   Christopher Rudel
+*   Professor Xu
+*   CS 392
+*   cs392_string.h
+*   I pledge my honor that I have abided by the Stevens Honor System
+*
+*/
+
+#ifndef CS392_STRING_H
+#define CS392_STRING_H
+
+void *cs392_memcpy (void *, void *, unsigned);
+unsigned cs392_strlen(char *);
+
+#endif
---
+++ cs392/as2/cs392_strlen.c
@@ -0,0 +1,18 @@
+/*
+*   Christopher Rudel
+*   Professor Xu
+*   CS 392
+*   cs392_strlen.c
+*   I pledge my honor that I have abided by the Stevens Honor System
+*
+*/
+
+#include "cs392_string.h"
+
+unsigned cs392_strlen(char *str){
+    unsigned len = 0;
+    for(int i=0; str[i] != '\0'; i++){
+        ++len;
+    }
+    return len;
+}
